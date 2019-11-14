63f19ddcaf38d2c510922124a2cd819242d6dafb
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-11-14 12:16:58 +0000

    first commit - text input, button and text view added to UI

--- BigApp/ViewController.swift
+++ BigApp/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var keywordField: UITextField!
+    @IBOutlet weak var complimentView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        keywordField.delegate = self
+        
     }
 
 
+
+    @IBAction func goTapped(_ sender: Any) {
+        print("button was tapped")
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
